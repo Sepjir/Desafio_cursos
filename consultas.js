7ab0bfab42f90fe86ab8d04cd2a476a18ecb7118
ignacio <ignacio.moenen@gmail.com>
2022-03-17 17:09:31 -0300

    challenge completed

---
+++ consultas.js
@@ -0,0 +1,47 @@
+const {Pool} = require("pg")
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    password: "nemesis",
+    database: "cursos",
+    port: 5432,
+})
+
+async function nuevoCurso(nombre, nivel, fecha, duracion) {
+    try {
+        const result = await pool.query(`INSERT INTO cursos (nombre, nivel, fecha, duracion) VALUES ('${nombre}', ${nivel}, '${fecha}', ${duracion}) RETURNING*;`)
+        return result.rows
+    } catch (e) {
+        return e
+    }
+}
+
+async function getCurso() {
+    try {
+        const result = await pool.query("SELECT * FROM cursos;")
+        return result.rows
+    } catch (e) {
+        return e
+    }
+}
+
+async function editCurso(id, nombre, nivel, fecha, duracion) {
+    try {
+        const result = await pool.query(`UPDATE cursos SET nombre = '${nombre}', nivel = ${nivel}, fecha = '${fecha}', duracion = ${duracion} WHERE id = ${id} RETURNING*`)
+        return result.rows
+    } catch (e) {
+        return e
+    }
+}
+
+async function eliminarCurso(id) {
+    try {
+        const result = await pool.query(`DELETE FROM cursos WHERE id = '${id}'`)
+        return result.rowCount
+    } catch (e) {
+        return e
+    }
+}
+
+
+module.exports = {nuevoCurso, getCurso, editCurso, eliminarCurso}
---
+++ index.js
@@ -0,0 +1,46 @@
+//Instanciando modulos de Express, Body-Parser y consultas del modulo PG
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const {nuevoCurso, getCurso, editCurso, eliminarCurso} = require("./consultas")
+
+//Configuracion de Body-Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Disponibilizando ruta raíz para consumo del archivo index.html
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+//ruta POST para agregar cursos a la base de datos en Postgres
+app.post("/curso", async (req, res) => {
+    const {nombre, nivelTecnico, fechaInicio, duracion} = req.body
+    const respuesta = await nuevoCurso(nombre, nivelTecnico, fechaInicio, duracion)
+    res.send(respuesta)
+})
+
+//Ruta GET para mostrar los cursos de la base de datos
+app.get("/cursos", async (req, res) => {
+    const respuesta = await getCurso()
+    res.send(respuesta)
+})
+
+//Ruta PUT para modificar los datos de la base de datos
+app.put("/curso", async (req, res) => {
+    const {id, nombre, nivelTecnico, fechaInicio, duracion} = req.body
+    const respuesta = await editCurso(id, nombre, nivelTecnico, fechaInicio, duracion)
+})
+
+//Ruta DELETE para eliminar los cursos según su ID
+app.delete("/curso/:id", async (req, res) => {
+    const {id} = req.params
+    const respuesta = eliminarCurso(id)
+
+    respuesta > 0
+        ?res.send(`El canal con id '${id}' fue borrado con éxito`)
+        :res.send(`El canal con id '${id}' no está registrado`)
+})
+
+//Escuchando el puerto 3000
+app.listen(3000, () => console.log("Servidor levantado en puerto 3000"))
